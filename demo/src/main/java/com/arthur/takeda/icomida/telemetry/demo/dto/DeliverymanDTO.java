bc54f6d86e513810ec8530b2d72fbba7967a9504
Arthur Gustavo Takeda <arthur-takeda@arthur.takeda>
2021-01-04 16:42:50 -0300

    Fixes on Deliveryman

--- demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/dto/DeliverymanDTO.java
+++ demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/dto/DeliverymanDTO.java
@@ -1,4 +1,70 @@
 package com.arthur.takeda.icomida.telemetry.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Locale;
+
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class DeliverymanDTO {
+
+    private Long deliverymanId;
+    private String name;
+    private String cpf;
+    private String status;
+    private String batteryStatus;
+
+    public DeliverymanDTO(Long deliverymanId, String name, String cpf, String status, String batteryStatus) {
+        this.deliverymanId = deliverymanId;
+        this.name = name;
+        this.cpf = cpf;
+        this.status = status;
+        this.batteryStatus = batteryStatus;
+    }
+
+    public DeliverymanDTO() {
+    }
+
+    public Long getDeliverymanId() {
+        return deliverymanId;
+    }
+
+    public void setDeliverymanId(Long deliverymanId) {
+        this.deliverymanId = deliverymanId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBatteryStatus() {
+        return batteryStatus;
+    }
+
+    public void setBatteryStatus(String batteryStatus) {
+        this.batteryStatus = batteryStatus;
+    }
 }
--- demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/service/DeliverymanService.java
+++ demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/service/DeliverymanService.java
@@ -1,8 +1,17 @@
 package com.arthur.takeda.icomida.telemetry.demo.service;
 
+import com.arthur.takeda.icomida.telemetry.demo.dto.DeliverymanDTO;
+import com.arthur.takeda.icomida.telemetry.demo.dto.mapper.DeliverymanMapper;
 import com.arthur.takeda.icomida.telemetry.demo.model.Deliveryman;
+import com.arthur.takeda.icomida.telemetry.demo.repository.DeliverymanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class DeliverymanService {
@@ -10,8 +19,63 @@ public class DeliverymanService {
     @Autowired
     private DeliverymanRepository deliverymanRepository;
 
-    public Deliveryman getById(Long id){
-        return
+    @Autowired
+    private DeliverymanMapper mapper;
+
+    public DeliverymanDTO getById(Long id){
+        Optional<Deliveryman> deliveryman = deliverymanRepository.findByDeliverymanIdAndActive(id, Boolean.TRUE);
+
+        if(deliveryman.isEmpty()){
+            return new DeliverymanDTO();
+        }
+
+        return mapper.toDeliverymanDTO(deliveryman.get());
+    }
+
+    public List<DeliverymanDTO> findAll(){
+        List<Deliveryman> deliverymanList = deliverymanRepository.findAllByActive(Boolean.TRUE);
+
+        return deliverymanList
+                .parallelStream()
+                .map(mapper::toDeliverymanDTO)
+                .collect(Collectors.toList());
+    }
+
+    public Long save(DeliverymanDTO deliverymanDTO){
+        Deliveryman deliveryman = mapper.toDeliveryman(deliverymanDTO);
+
+        deliverymanRepository.save(deliveryman);
+
+        return deliveryman.getDeliverymanId();
+    }
+
+    public Long save(Long id, DeliverymanDTO deliverymanDTO) throws Exception {
+        Optional<Deliveryman> deliverymanRecord = deliverymanRepository.findById(id);
+        Deliveryman deliveryman;
+
+        if(deliverymanRecord.isEmpty()){
+            throw new Exception("Deliveryman not found");
+        }
+
+        deliverymanDTO.setDeliverymanId(id);
+
+        deliverymanRepository.save(mapper.toDeliveryman(deliverymanDTO));
+
+        return deliverymanRecord.get().getDeliverymanId();
+    }
+
+    @Transactional
+    public void delete(Long id) throws Exception {
+        Optional<Deliveryman> deliverymanRecord = deliverymanRepository.findByDeliverymanIdAndActive(id, Boolean.TRUE);
+        Deliveryman deliveryman;
+
+        if(deliverymanRecord.isEmpty()){
+            throw new Exception("Deliveryman not found");
+        }
+
+        deliveryman = deliverymanRecord.get();
+
+        deliveryman.setActive(Boolean.FALSE);
     }
 
 }
