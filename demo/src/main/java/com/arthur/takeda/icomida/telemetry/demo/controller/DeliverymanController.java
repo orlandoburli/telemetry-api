a49b17b01951269b544663edf6cd662063c4852c
Arthur Gustavo Takeda <arthur-takeda@arthur.takeda>
2021-01-04 16:42:50 -0300

    Fixes on Deliveryman

--- demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/controller/DeliverymanController.java
+++ demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/controller/DeliverymanController.java
@@ -1,7 +1,60 @@
 package com.arthur.takeda.icomida.telemetry.demo.controller;
 
-@
+import com.arthur.takeda.icomida.telemetry.demo.dto.DeliverymanDTO;
+import com.arthur.takeda.icomida.telemetry.demo.service.DeliverymanService;
+import com.sun.net.httpserver.Headers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/deliveryman")
 public class DeliverymanController {
 
+    @Autowired
+    DeliverymanService deliverymanService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity findById(@PathVariable Long id){
+        return ResponseEntity.ok(deliverymanService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity findAll(){
+        return ResponseEntity.ok(deliverymanService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody DeliverymanDTO deliverymanDTO){
+        Long id = deliverymanService.save(deliverymanDTO);
+
+        return ResponseEntity.created(URI.create("" + id)).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity save(@PathVariable Long id, @RequestBody DeliverymanDTO deliverymanDTO){
+        try {
+            deliverymanService.save(id, deliverymanDTO);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        try{
+            deliverymanService.delete(id);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/model/Deliveryman.java
+++ demo/src/main/java/com/arthur/takeda/icomida/telemetry/demo/model/Deliveryman.java
@@ -7,9 +7,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Deliveryman {
 
     @Id
@@ -25,4 +25,67 @@ public class Deliveryman {
 
     @Column(name = "battery_status")
     private String batteryStatus;
+
+    private Boolean active;
+
+    public Deliveryman(Long deliverymanId, String cpf, String name, String status, String batteryStatus) {
+        this.deliverymanId = deliverymanId;
+        this.cpf = cpf;
+        this.name = name;
+        this.status = status;
+        this.batteryStatus = batteryStatus;
+        this.active = true;
+    }
+
+    public Deliveryman(){
+        this.active = true;
+    }
+
+    public Long getDeliverymanId() {
+        return deliverymanId;
+    }
+
+    public void setDeliverymanId(Long deliverymanId) {
+        this.deliverymanId = deliverymanId;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBatteryStatus() {
+        return batteryStatus;
+    }
+
+    public void setBatteryStatus(String batteryStatus) {
+        this.batteryStatus = batteryStatus;
+    }
+
+    public void setActive(Boolean active){
+        this.active = active;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
 }
